999520eb3e85d8334587edc0eae4ba9d33eaffb5
Carl Scotney <carl@unlockeddata.com>
2024-09-04 17:34:33 +0100

    dotnet 8 update and made library parse values before passing

--- src/wwwroot/blazorNoUISliderInterop.js
+++ src/wwwroot/blazorNoUISliderInterop.js
@@ -1,40 +1,59 @@
 ﻿function addNoUiSlider(options, element, dotNetRef) {
-    
+
     noUiSlider.create(element, options)
     element.noUiSlider.dotNetRef = dotNetRef;
 
 
     element.noUiSlider.on('end', function (values, handle, unencoded, tap, positions, noUiSlider) {
-        noUiSlider.dotNetRef.invokeMethodAsync("SliderEnd", unencoded, handle);
+        invokeDotnetMethod(noUiSlider,"SliderEnd", unencoded, handle);
+
     });
     element.noUiSlider.on('update', function (values, handle, unencoded, tap, positions, noUiSlider) {
-        noUiSlider.dotNetRef.invokeMethodAsync("SliderUpdate", unencoded, handle);
+        invokeDotnetMethod(noUiSlider,"SliderUpdate", unencoded, handle);
+
     });
     element.noUiSlider.on('set', function (values, handle, unencoded, tap, positions, noUiSlider) {
-        noUiSlider.dotNetRef.invokeMethodAsync("SliderSet", unencoded, handle);
+        invokeDotnetMethod(noUiSlider,"SliderSet", unencoded, handle);
+
     });
     element.noUiSlider.on('change', function (values, handle, unencoded, tap, positions, noUiSlider) {
-        noUiSlider.dotNetRef.invokeMethodAsync("SliderChange", unencoded, handle);
+        invokeDotnetMethod(noUiSlider,"SliderChange", unencoded, handle);
+
     });
     element.noUiSlider.on('start', function (values, handle, unencoded, tap, positions, noUiSlider) {
-        noUiSlider.dotNetRef.invokeMethodAsync("SliderStart", unencoded, handle);
+        invokeDotnetMethod(noUiSlider,"SliderStart", unencoded, handle);
+
     });
     element.noUiSlider.on('slide', function (values, handle, unencoded, tap, positions, noUiSlider) {
-        noUiSlider.dotNetRef.invokeMethodAsync("SliderSlide", unencoded, handle);
+        invokeDotnetMethod(noUiSlider,"SliderSlide", unencoded, handle);
+
     });
 };
+
+function invokeDotnetMethod(noUiSlider,methodName, value, handle) {
+    console.log("invokeDotnetMethod");
+    if (Array.isArray(value)) {
+        value.forEach(function (item, index) {
+            value[index] = parseInt(item);
+        });
+    } else {
+        value = parseInt(value);
+    }
+
+    noUiSlider.dotNetRef.invokeMethodAsync(methodName, value, handle);
+}
 
 function updateNoUiSlider(option, element, dotNetRef) {
     //assume two handled for now, update for multi as needed
     //no ui can't update the start and end
-    
+
     if (element.noUiSlider.options.start[0] != option.start[0] || element.noUiSlider.options.start[1] != option.start[1]) {
-      element.noUiSlider.destroy();
+        element.noUiSlider.destroy();
         addNoUiSlider(option, element, dotNetRef);
-        
-    } 
-   
-    
-   
-    
+
+    }
+
+
+
+
 }
